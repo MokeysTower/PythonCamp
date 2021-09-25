ba89a2ea118857be63a1989c19390171f0d3ed53
MonkeysTower <Trifandre@yandex.ru>
2021-09-25 18:01:01 +0500

    feat: Add BMI.py and math.py

---
+++ Tasks/bmi.py
@@ -0,0 +1,3 @@
+mass = float(input('Write your weight (kg) '))
+tall = pow(float(input('write your tall (meters) ')), 2)
+print('BMI=', mass / tall)
---
+++ Tasks/math.py
@@ -0,0 +1,5 @@
+import math
+print(min(7,10,9,20))
+print(min(-1,2,5,-1.5))
+print(min(23532,35329,2335,33))
+print(int(math.sqrt(int(input('Put some number ')))))
