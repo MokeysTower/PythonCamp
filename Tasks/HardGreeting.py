884e7a60fc97ad78b1cd1f1aa7d275b2ff663dfa
MonkeysTower <Trifandre@yandex.ru>
2021-09-25 21:13:25 +0500

    feat: add loop in HardGreeting

--- Tasks/HardGreeting.py
+++ Tasks/HardGreeting.py
@@ -1,17 +1,23 @@
-name = input('Write yuor name ')
-if name=='Ann':
-    print('Hello, '.lower(), name,'! Did you want to see new products?'.lower())
-elif name=='Abby' or name=='Garry':
-    secondname = input('Name of togeherwyaer ')
-    if secondname=='Abby' or secondname=='Garry':
-        print('Hello, Good boys! Did you want to see new products?')
-    else:
+while True:
+    name = input('Write yuor name ')
+    gre = 'Hello, 1! Did you want to see new products?'
+    if name=='Ann':
+        gre = gre.lower() 
+        print(gre.replace('1', name))
+    elif name=='Abby' or name=='Garry':
+        secondname = input('Name of togeherwyaer ')
+        if secondname=='Abby' or secondname=='Garry':
+            print(gre.replace('1', 'Good Boys'))
+        else:
+            print(gre.replace('1', name))
+    elif name=='Braver':
+        gre = gre.upper() 
+        print(gre.replace('1', name))
+    elif name=='HeadChop' or name=='Headchop':
         print(f'Hello, {name}! Did you want to see new products?')
-elif name=='Braver':
-    print('Hello, '.upper(), name,'! Did you want to see new products?'.upper())
-elif name=='HeadChop':
-    print(f'Hello, {name}! Did you want to see new products?')
-else:
-    print('Hello! Can I help you?')
+    elif name=='out':
+        break
+    else:
+        print('Hello! Can I help you?')
 
 
