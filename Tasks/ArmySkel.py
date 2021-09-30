adc488536644ec6752c9fdfc24321002d3098e2f
MonkeysTower <Trifandre@yandex.ru>
2021-09-30 23:22:42 +0500

    feat: upgrade ArmySkel

--- Tasks/ArmySkel.py
+++ Tasks/ArmySkel.py
@@ -21,7 +21,7 @@ def deletiSkel(z):
             continue
         if Army[i] == z:
             skelt: dict = Army[i] 
-            skelt.pop()
+            skelt.clear()
     return
     
 def alfind(id, Army):
@@ -39,19 +39,22 @@ def fight(x):
         return
     #raunds
     while True:
+        if hero['health'] <= 0:
+            print('Hero dead')
+            return
         z +=1
         print('\n\nРаунд', z)
         #Hero attacks
         Army[x-1]['health'] = Army[x-1]['health'] - hero['damage']
         if Army[x-1]['health'] == 0:
             skelt: dict = Army[x-1] 
-            skelt.pop()
+            skelt.clear()
             currHealth = hero['health']
             print(f'\n\nSkeleton can attak, he die after\nHero:\nHealth:{currHealth}\n')
             return
         elif Army[x-1]['health'] != 0:
             hero['health'] -= Army[x-1]['damage']
-            if hero['health'] == 0:
+            if hero['health'] <= 0:
                 currHealth = Army[x-1]['health']
                 print(f'\n\nHero dead.\n\nSkeleton:\nHealth:{currHealth}\n')
                 return
