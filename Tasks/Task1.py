e7e2b93066c22923aa326f91cdb9cd66fba2f321
MonkeysTower <Trifandre@yandex.ru>
2021-10-02 16:25:48 +0500

    refactor: change metod Task1.py

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -1,15 +1,11 @@
 def addmembers(q):
     z=[]
+    f = 0
     for i in range(q):
         if i != 0 and i%3 == 0 or i != 0 and i%5 == 0:
             z.append(i)
-    return z
-
-def calculation(z):
-    q = 0
     for i in range(len(z)):
-        q += z[i]
-    return(q)
+        f += z[i]
+    return(f)
 
-t = int(input('Write the number\n'))
-print(calculation(addmembers(t)))
+
